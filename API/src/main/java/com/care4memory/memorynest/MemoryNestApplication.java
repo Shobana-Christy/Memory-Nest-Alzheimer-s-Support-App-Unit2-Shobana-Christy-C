09ec305b9dd2e5afb1d8aac3f451b04841c0aa51
Shobana-Christy <shobichristy07@gmail.com>
2026-02-24 23:02:05 -0600

    First Commit to main

---
+++ API/src/main/java/com/care4memory/memorynest/MemoryNestApplication.java
@@ -0,0 +1,13 @@
+package com.care4memory.memorynest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryNestApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/com/care4memory/memorynest/MemoryNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care4memory.memorynest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
