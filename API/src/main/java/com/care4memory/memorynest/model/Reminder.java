ce231241981d6abbe674de2bcbb396f2219d39c5
Shobana-Christy <shobichristy07@gmail.com>
2026-02-27 20:23:53 -0600

    created model for reminder

--- API/src/main/java/com/care4memory/memorynest/model/Reminder.java
+++ API/src/main/java/com/care4memory/memorynest/model/Reminder.java
@@ -1,4 +1,77 @@
 package com.care4memory.memorynest.model;
 
-public class reminder {
+import jakarta.persistence.*;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+public class Reminder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer reminderId;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_role_id", nullable = false)
+//    private UserRole userRoleId;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+    private String notes;
+
+    public Reminder() {
+    }
+
+    public Reminder(Integer reminderId, String name, LocalDate date, LocalTime time, String notes) {
+        this.reminderId = reminderId;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.notes = notes;
+
+    }
+
+    public Integer getReminderId() {
+        return reminderId;
+    }
+
+    public void setReminderId(Integer reminderId) {
+        this.reminderId = reminderId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
+
