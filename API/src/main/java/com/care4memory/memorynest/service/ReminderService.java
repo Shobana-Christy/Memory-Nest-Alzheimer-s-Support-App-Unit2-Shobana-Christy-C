711eb61e24df9a2f42e1dd62ed938a5505737c59
Shobana-Christy <shobichristy07@gmail.com>
2026-02-27 20:28:00 -0600

    created service class for reminder

--- API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
+++ API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
@@ -1,4 +1,38 @@
 package com.care4memory.memorynest.service;
 
+import com.care4memory.memorynest.model.Reminder;
+import com.care4memory.memorynest.repositories.ReminderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReminderService {
+
+    @Autowired
+    private ReminderRepository reminderRepository;
+
+
+    public List<Reminder> viewReminders() {
+        return reminderRepository.findAll();
+    }
+
+    public Reminder getReminderById(Integer id){
+        return reminderRepository.findById(id).orElse(null);
+    }
+
+    public Reminder addReminder(Reminder reminder) {
+        return reminderRepository.save(reminder);
+    }
+
+    public Reminder updateReminder(Integer id, Reminder reminder) {
+        reminder.setReminderId(id);
+        return reminderRepository.save(reminder);
+    }
+
+    public void deleteReminder(Integer id) {
+        reminderRepository.deleteById(id);
+    }
 }
+
