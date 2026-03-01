339d0954b6f8bd283616b23966a0178599f29f8e
Shobana-Christy <shobichristy07@gmail.com>
2026-03-01 13:46:08 -0600

    added changes in type

--- API/src/main/java/com/care4memory/memorynest/repositories/ReminderRepository.java
+++ API/src/main/java/com/care4memory/memorynest/repositories/ReminderRepository.java
@@ -1,7 +1,7 @@
 package com.care4memory.memorynest.repositories;
 
-import com.care4memory.memorynest.model.Reminder;
+import com.care4memory.memorynest.model.ReminderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReminderRepository extends JpaRepository <Reminder, Integer> {
+public interface ReminderRepository extends JpaRepository <ReminderEntity, Long> {
 }
