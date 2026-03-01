cb2a51dab80ed4d2047a64502c5a1ce6b7ed3f82
Shobana-Christy <shobichristy07@gmail.com>
2026-03-01 15:06:05 -0600

    created engagement center DTO

--- API/src/main/java/com/care4memory/memorynest/dto/EngagementCenterDTO.java
+++ API/src/main/java/com/care4memory/memorynest/dto/EngagementCenterDTO.java
@@ -1,4 +1,41 @@
 package com.care4memory.memorynest.dto;
 
-public class EngagementCenter {
+public class EngagementCenterDTO {
+
+    private Long engagementCenterId;
+    private String name;
+    private String src;
+
+    public EngagementCenterDTO() {
+    }
+
+    public EngagementCenterDTO(Long engagementCenterId, String name, String src) {
+        this.engagementCenterId = engagementCenterId;
+        this.name = name;
+        this.src = src;
+    }
+
+    public Long getEngagementCenterId() {
+        return engagementCenterId;
+    }
+
+    public void setEngagementCenterId(Long engagementCenterId) {
+        this.engagementCenterId = engagementCenterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
 }
