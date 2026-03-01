038b122e62bcd2bf2f9eb4307e1e47263b7afbf2
Shobana-Christy <shobichristy07@gmail.com>
2026-03-01 15:58:12 -0600

    added changes in controller methods with ResponseEntity

--- API/src/main/java/com/care4memory/memorynest/controller/ContactUsController.java
+++ API/src/main/java/com/care4memory/memorynest/controller/ContactUsController.java
@@ -1,7 +1,9 @@
 package com.care4memory.memorynest.controller;
 
-import com.care4memory.memorynest.model.ContactUs;
-import com.care4memory.memorynest.service.ContactService;
+import com.care4memory.memorynest.dto.ContactUsDTO;
+import com.care4memory.memorynest.model.ContactUsEntity;
+import com.care4memory.memorynest.service.ContactUsService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contactus")
-public class ContactController {
+public class ContactUsController {
 
-    private final ContactService contactService;
+    private final ContactUsService contactUsService;
 
-    public ContactController(ContactService contactService) {
-        this.contactService = contactService;
+    public ContactUsController(ContactUsService contactUsService) {
+        this.contactUsService = contactUsService;
     }
 
     @PostMapping
-    public ContactUs addContact(@RequestBody ContactUs contactUs) {
-        return contactService.addContact(contactUs);
+    public ResponseEntity<ContactUsDTO> addContact(@RequestBody ContactUsDTO contactUsDTO) {
+        ContactUsDTO savedContact = this.contactUsService.addContact(contactUsDTO);
+        return ResponseEntity.status(201).body(savedContact); // Return the saved contact with HTTP 201 Created status
+
     }
 }
