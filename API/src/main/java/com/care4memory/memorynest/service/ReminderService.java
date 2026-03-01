0540b6a3f9e6c958198e8d44e6809552ad4db108
Shobana-Christy <shobichristy07@gmail.com>
2026-03-01 13:45:02 -0600

    added methods in reminder service

--- API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
+++ API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
@@ -1,6 +1,7 @@
 package com.care4memory.memorynest.service;
 
-import com.care4memory.memorynest.model.Reminder;
+import com.care4memory.memorynest.dto.ReminderDTO;
+import com.care4memory.memorynest.model.ReminderEntity;
 import com.care4memory.memorynest.repositories.ReminderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,25 +15,55 @@ public class ReminderService {
     private ReminderRepository reminderRepository;
 
 
-    public List<Reminder> viewReminders() {
-        return reminderRepository.findAll();
+    public List<ReminderDTO> viewReminders() {
+        return reminderRepository.findAll()
+                .stream()
+                .map(this::convertToDto)   //(re) => convertToDto(re)
+                .toList();
+
     }
 
-    public Reminder getReminderById(Integer id){
-        return reminderRepository.findById(id).orElse(null);
+    public ReminderDTO getReminderById(Long reminderId) {
+        return reminderRepository.findById(reminderId)
+                        .map(this::convertToDto)
+                        .orElse(null);
     }
 
-    public Reminder addReminder(Reminder reminder) {
-        return reminderRepository.save(reminder);
+    public ReminderDTO addReminder(ReminderDTO reminderDTO) {
+        ReminderEntity entity = convertToEntity(reminderDTO);
+        ReminderEntity savedEntity = reminderRepository.save(entity);
+        return convertToDto(savedEntity);
     }
 
-    public Reminder updateReminder(Integer id, Reminder reminder) {
-        reminder.setReminderId(id);
-        return reminderRepository.save(reminder);
+    public ReminderDTO updateReminder(Long reminderId, ReminderDTO reminderDTO) {
+       ReminderEntity entity = convertToEntity(reminderDTO);
+       entity.setReminderId(reminderId);
+       ReminderEntity updatedEntity = reminderRepository.save(entity);
+         return convertToDto(updatedEntity);
     }
 
-    public void deleteReminder(Integer id) {
-        reminderRepository.deleteById(id);
+    public void deleteReminder(Long reminderId) {
+        reminderRepository.deleteById(reminderId);
+    }
+
+    private ReminderDTO convertToDto(ReminderEntity reminderEntity) {
+        ReminderDTO reminderDTO = new ReminderDTO();
+        reminderDTO.setReminderId(reminderEntity.getReminderId());
+        reminderDTO.setName(reminderEntity.getName());
+        reminderDTO.setDate(reminderEntity.getDate());
+        reminderDTO.setTime(reminderEntity.getTime());
+        reminderDTO.setNotes(reminderEntity.getNotes());
+        return reminderDTO;
+    }
+
+    private ReminderEntity convertToEntity(ReminderDTO reminderDTO) {
+        ReminderEntity reminderEntity = new ReminderEntity();
+        reminderEntity.setReminderId(reminderDTO.getReminderId());
+        reminderEntity.setName(reminderDTO.getName());
+        reminderEntity.setDate(reminderDTO.getDate());
+        reminderEntity.setTime(reminderDTO.getTime());
+        reminderEntity.setNotes(reminderDTO.getNotes());
+        return reminderEntity;
     }
 }
 
