9a13f653b018ca43c730f10d6e974d76cf0aff42
Shobana-Christy <shobichristy07@gmail.com>
2026-03-02 00:07:54 -0600

    added changes in user role in reminder service

--- API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
+++ API/src/main/java/com/care4memory/memorynest/service/ReminderService.java
@@ -1,9 +1,11 @@
 package com.care4memory.memorynest.service;
 
 import com.care4memory.memorynest.dto.ReminderDTO;
+import com.care4memory.memorynest.dto.UserRoleDTO;
 import com.care4memory.memorynest.model.ReminderEntity;
+import com.care4memory.memorynest.model.UserRoleEntity;
 import com.care4memory.memorynest.repositories.ReminderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.care4memory.memorynest.repositories.UserRoleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,16 +13,19 @@ import java.util.List;
 @Service
 public class ReminderService {
 
-    @Autowired
     private ReminderRepository reminderRepository;
+    private final UserRoleRepository userRoleRepository;
 
+    public ReminderService(ReminderRepository reminderRepository, UserRoleRepository userRoleRepository) {
+        this.reminderRepository = reminderRepository;
+        this.userRoleRepository = userRoleRepository;
+    }
 
     public List<ReminderDTO> viewReminders() {
         return this.reminderRepository.findAll()
                 .stream()
                 .map(this::convertToDto)   //(re) => convertToDto(re)
                 .toList();
-
     }
 
     public ReminderDTO getReminderById(Long reminderId) {
@@ -29,15 +34,17 @@ public class ReminderService {
                 .orElse(null);
     }
 
-    public ReminderDTO addReminder(ReminderDTO reminderDTO) {
+    public ReminderDTO addReminder(ReminderDTO reminderDTO, UserRoleDTO userRoleDTO) {
         ReminderEntity entity = convertToEntity(reminderDTO);
+        entity.setUserRole(convertToUserRoleEntity(userRoleDTO));
         ReminderEntity savedEntity = this.reminderRepository.save(entity);
         return convertToDto(savedEntity);
     }
 
-    public ReminderDTO updateReminder(Long reminderId, ReminderDTO reminderDTO) {
+    public ReminderDTO updateReminder(Long reminderId, ReminderDTO reminderDTO, UserRoleDTO userRoleDTO) {
         ReminderEntity entity = convertToEntity(reminderDTO);
         entity.setReminderId(reminderId);
+        entity.setUserRole(convertToUserRoleEntity(userRoleDTO));
         ReminderEntity updatedEntity = this.reminderRepository.save(entity);
         return convertToDto(updatedEntity);
     }
@@ -53,6 +60,7 @@ public class ReminderService {
         reminderDTO.setDate(reminderEntity.getDate());
         reminderDTO.setTime(reminderEntity.getTime());
         reminderDTO.setNotes(reminderEntity.getNotes());
+        reminderDTO.setUserRoleId(reminderEntity.getUserRole().getUserRoleId());
         return reminderDTO;
     }
 
@@ -63,7 +71,16 @@ public class ReminderService {
         reminderEntity.setDate(reminderDTO.getDate());
         reminderEntity.setTime(reminderDTO.getTime());
         reminderEntity.setNotes(reminderDTO.getNotes());
+
         return reminderEntity;
+    }
+
+    private UserRoleEntity convertToUserRoleEntity(UserRoleDTO userRoleDTO) {
+        UserRoleEntity userRoleEntity = new UserRoleEntity();
+        userRoleEntity.setUserRoleId(userRoleDTO.getUserRoleId());
+        userRoleEntity.setRole(userRoleDTO.getRole());
+        userRoleEntity.setEmail(userRoleDTO.getEmail());
+        return userRoleEntity;
     }
 }
 
