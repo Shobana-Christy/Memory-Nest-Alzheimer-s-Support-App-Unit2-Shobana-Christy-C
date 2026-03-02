8c975b1037e59a03c0128f51f60c4dc0dc5ea591
Shobana-Christy <shobichristy07@gmail.com>
2026-03-02 09:14:37 -0600

    created AlbumDTO

--- API/src/main/java/com/care4memory/memorynest/dto/AlbumDTO.java
+++ API/src/main/java/com/care4memory/memorynest/dto/AlbumDTO.java
@@ -1,4 +1,22 @@
 package com.care4memory.memorynest.dto;
 
-public class Album {
+public class AlbumDTO {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
