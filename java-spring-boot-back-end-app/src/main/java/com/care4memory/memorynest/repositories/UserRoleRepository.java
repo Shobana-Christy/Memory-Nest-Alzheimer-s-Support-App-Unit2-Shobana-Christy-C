b73ca0187efb29bac25579ca00032a57405e00cc
Shobana-Christy <shobichristy07@gmail.com>
2026-03-09 09:49:23 -0500

    Added findByEmail method

--- java-spring-boot-back-end-app/src/main/java/com/care4memory/memorynest/repositories/UserRoleRepository.java
+++ java-spring-boot-back-end-app/src/main/java/com/care4memory/memorynest/repositories/UserRoleRepository.java
@@ -4,4 +4,5 @@ import com.care4memory.memorynest.model.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+    java.util.Optional<UserRoleEntity> findByEmail(String email);
 }
