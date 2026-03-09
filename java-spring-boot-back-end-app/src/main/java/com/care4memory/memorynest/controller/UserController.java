31d71b9bbd856b0719b08b7473907fc91bc49d3c
Shobana-Christy <shobichristy07@gmail.com>
2026-03-09 14:13:02 -0500

    Added method to get userInfo

--- java-spring-boot-back-end-app/src/main/java/com/care4memory/memorynest/controller/UserController.java
+++ java-spring-boot-back-end-app/src/main/java/com/care4memory/memorynest/controller/UserController.java
@@ -11,23 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-roles")
-public class UserRoleContoller {
+@RequestMapping("/user")
+public class UserController {
 
     private  final UserRoleService userRoleService;
 
-    public UserRoleContoller(UserRoleService userRoleService) {
+    public UserController(UserRoleService userRoleService) {
         this.userRoleService = userRoleService;
     }
 
     @GetMapping
+    public ResponseEntity<UserRoleDTO> getUserInfo() {
+        UserRoleDTO user = userRoleService.getUserInfo();
+        if(user != null) {
+            // Return the user information with HTTP 200 OK status
+            return ResponseEntity.ok().body(user);
+
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/roles")
     public ResponseEntity<List<UserRoleDTO>> getAllUserRoles() {
         List<UserRoleDTO> userRoles = this.userRoleService.getAllUserRoles();
         // Return the list of user roles with HTTP 200 OK status
         return ResponseEntity.ok(userRoles);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/roles/{id}")
     public ResponseEntity<UserRoleDTO> getUserRoleById(@PathVariable Long id){
         UserRoleDTO userRole = this.userRoleService.getUserRoleById(id);
         if(userRole == null){
