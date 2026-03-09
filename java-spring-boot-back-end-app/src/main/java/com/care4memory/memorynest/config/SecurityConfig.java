6a916ddc6c791780efc8e71aeffef0bf4ee74309
Shobana-Christy <shobichristy07@gmail.com>
2026-03-09 11:32:08 -0500

    Added SecurityConfig class

---
+++ java-spring-boot-back-end-app/src/main/java/com/care4memory/memorynest/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.care4memory.memorynest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> {})   // <-- REQUIRED
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // enables Google login
+                .oauth2Login(oauth -> {
+                        oauth.loginPage("/oauth2/authorization/google");
+                        oauth.defaultSuccessUrl("http://localhost:5173/home", true);
+                })
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                );
+
+        return http.build();
+    }
+}
