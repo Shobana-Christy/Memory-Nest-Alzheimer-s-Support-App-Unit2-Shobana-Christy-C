f61f1a1ab5395f4da1635e22f1094980c3a7500c
Shobana-Christy <shobichristy07@gmail.com>
2026-03-11 12:33:31 -0500

    Added useEffect to fetch album content

--- react-front-end-app/src/components/pages/memoryspot/AlbumDetail.jsx
+++ react-front-end-app/src/components/pages/memoryspot/AlbumDetail.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router";
+import { useState } from "react";
+import "./memoryspot.css"
+import LoadingPage from "../LoadingPage";
+import { useEffect } from "react";
+import { fetchAlbumContent } from "../../common/dataCollection";
 
 const AlbumDetail = ({ album }) => {
+
+    let [albumContent, setAlbumContent] = useState([]);
+
+    useEffect(() => {
+        console.log("album: ", album);
+        let albumContentPromise = fetchAlbumContent(album.name);
+        albumContentPromise.then((contentAsList) => {
+            setAlbumContent(contentAsList);
+        });
+    }, [album]);
 
     return (
         <>
         <div className="album-container">
             {
-                album.src.map((url, index) => 
-                        (<img src={url} alt={album.name} key={index} className="album-picture"/>)
+                albumContent.map((conent, index) =>
+                        (<img src={conent.url} alt={conent.name} key={index} className="album-picture" />)
                 )
             }
             </div>
