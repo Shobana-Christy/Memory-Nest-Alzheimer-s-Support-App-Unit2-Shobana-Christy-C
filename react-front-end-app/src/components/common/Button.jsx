9b514c5b10270548f5291d1355c23dbadf9f5433
Shobana-Christy <shobichristy07@gmail.com>
2026-03-14 18:37:47 -0500

    Added type attribute

--- react-front-end-app/src/components/common/Button.jsx
+++ react-front-end-app/src/components/common/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ id, name, label, value, classes,onClick }) => {
+const Button = ({ id, name, label, value, type, classes,onClick }) => {
 
     return (
-        <button id={id} name={name} value={value} className={classes? classes: "button-class"} onClick={onClick}>
+        <button id={id} name={name} value={value} type={type} className={classes? classes: "button-class"} onClick={onClick}>
             {label}
         </button>
     );
